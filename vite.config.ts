8c2e0c8f59918f6be01480efc7755eea37644ee5
berlysia <berlysia@gmail.com>
2025-01-12 00:22:04 +0900

    setup ssg

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import build from '@hono/vite-build/cloudflare-pages'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
+import ssg from '@hono/vite-ssg'
 
 export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), build()]
+  plugins: [honox({ devServer: { adapter } }), ssg({entry: "./app/server.ts"})]
 })
