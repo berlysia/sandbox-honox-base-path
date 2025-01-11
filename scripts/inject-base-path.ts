bbb5176fd4f0085db60b1a742a426d101e97b91c
berlysia <berlysia@gmail.com>
2025-01-12 00:38:22 +0900

    implement base path injector

---
+++ scripts/inject-base-path.ts
@@ -0,0 +1,22 @@
+import fs from 'fs'
+import path from 'path'
+import type { Manifest, UserConfigExport } from 'vite'
+import viteConfig from '../vite.config.ts'
+
+const config: UserConfigExport = viteConfig;
+const calcuratedConfig = typeof config === "function" ? config({mode: "", command: "build"}) : config;
+const resolvedConfig = calcuratedConfig instanceof Promise ? await calcuratedConfig : calcuratedConfig;
+
+const basePath = resolvedConfig.base ?? '/'
+
+const manifestPath = path.resolve(import.meta.dirname, '../dist/.vite/manifest.json')
+const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8')) as Manifest
+
+Object.keys(manifest).forEach((assetName) => {
+  const file = manifest[assetName].file
+  manifest[assetName].file = basePath + file
+})
+
+fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2))
+
+console.log(`Injected base path ${basePath} into manifest.json`)
