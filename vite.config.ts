44f72f8c57f9ed511adaecf12f451f148d6007ca
berlysia <berlysia@gmail.com>
2025-01-12 01:39:27 +0900

    make tsconfig as solution style

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,29 @@ import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 import ssg from '@hono/vite-ssg'
+import client from 'honox/vite/client'
 
-export default defineConfig({
-  base: "/sandbox-honox-base-path/",
-  build: {
-    emptyOutDir: false,
-  },
-  plugins: [honox({ devServer: { adapter } }), ssg({entry: "./app/server.ts"})]
+export default defineConfig(({mode}) => {
+  if (mode === "client") {
+    return {
+      base: "/sandbox-honox-base-path/",
+      build: {
+        ssrManifest: true,
+      },
+      plugins: [client()]
+    }
+  }
+
+  return {
+    base: "/sandbox-honox-base-path/",
+    build: {
+      emptyOutDir: false,
+    },
+    plugins: [honox({ devServer: { adapter } }), ssg({entry: "./app/server.ts"})],
+    // maybe Vite does not support solution style tsconfig.json yet
+    esbuild: {
+      jsx: "automatic",
+      jsxImportSource: "hono/jsx",
+    }
+  }
 })
